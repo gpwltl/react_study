80a9eeba048cc4ba59edaf0d893e064b6919a1d9
gpwltl <gpwltl97@naver.com>
2020-08-12 20:15:42 +0900

    react-practice-hooks

---
+++ hooks/src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Average_useRef from "./Average_useRef";
+
+const App = () => {
+  // const [visible, setVisible] = useState(false);
+
+  // return (
+  //   <div>
+  //     <button
+  //       onClick={() => {
+  //         setVisible(!visible);
+  //       }}
+  //     >
+  //       {visible ? "숨기기" : "보이기"}
+  //     </button>
+  //     <hr />
+  //     {visible && <Info />}
+  //   </div>
+  // );
+
+  return <Average_useRef />;
+};
+
+export default App;
